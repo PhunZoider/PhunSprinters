6ce555bd5bb5b1ce6aeccdb7f52cceb934e342e0
PhunZoider <projectphunzoid@gmail.com>
2026-02-20 17:13:34 +0000

    watch distributions

--- Contents/mods/PhunSprintersUI/media/lua/server/PhunSprintersUI/items/distributions.lua
+++ Contents/mods/PhunSprintersUI/media/lua/server/PhunSprintersUI/items/distributions.lua
@@ -1,116 +1,122 @@
--- require 'Items/SuburbsDistributions'
--- require 'Vehicles/VehicleDistributions'
--- require "Items/ProceduralDistributions"
--- require 'Items/Distributions'
--- require 'Items/ItemPicker'
-if (SandboxVars.PhunSprintersUI or {}).AddToLoot then
+require 'Items/ProceduralDistributions'
 
-    -- ArmySurplusOutfit
-    table.insert(ProceduralDistributions.list["ArmySurplusOutfit"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ArmySurplusOutfit"].items, 1);
+local function preDistributionMerge()
 
-    -- ArmyStorageElectronics
-    table.insert(ProceduralDistributions.list["ArmyStorageElectronics"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ArmyStorageElectronics"].items, .05);
+    if (SandboxVars.PhunSprintersUI or {}).AddToLoot then
 
-    -- ArmySurplusMisc
-    table.insert(ProceduralDistributions.list["ArmySurplusMisc"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ArmySurplusMisc"].items, 1);
+        print("Adding PhunSprintersUI items to loot spawns...")
 
-    -- ElectronicStoreAppliances
-    table.insert(ProceduralDistributions.list["ElectronicStoreAppliances"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ElectronicStoreAppliances"].items, 2);
+        -- ArmySurplusOutfit
+        table.insert(ProceduralDistributions.list["ArmySurplusOutfit"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ArmySurplusOutfit"].items, 1);
 
-    -- ElectronicStoreMisc
-    table.insert(ProceduralDistributions.list["ElectronicStoreMisc"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ElectronicStoreMisc"].items, 2);
+        -- ArmyStorageElectronics
+        table.insert(ProceduralDistributions.list["ArmyStorageElectronics"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ArmyStorageElectronics"].items, .05);
 
-    -- FireDeptLockers
-    table.insert(ProceduralDistributions.list["FireDeptLockers"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["FireDeptLockers"].items, 1);
+        -- ArmySurplusMisc
+        table.insert(ProceduralDistributions.list["ArmySurplusMisc"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ArmySurplusMisc"].items, 1);
 
-    -- LockerArmyBedroom
-    table.insert(ProceduralDistributions.list["LockerArmyBedroom"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["LockerArmyBedroom"].items, 1);
+        -- ElectronicStoreAppliances
+        table.insert(ProceduralDistributions.list["ElectronicStoreAppliances"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ElectronicStoreAppliances"].items, 2);
 
-    -- MedicalStorageTools
-    table.insert(ProceduralDistributions.list["MedicalStorageTools"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["MedicalStorageTools"].items, 1);
+        -- ElectronicStoreMisc
+        table.insert(ProceduralDistributions.list["ElectronicStoreMisc"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ElectronicStoreMisc"].items, 2);
 
-    -- SecurityLockers
-    table.insert(ProceduralDistributions.list["SecurityLockers"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["SecurityLockers"].items, 0.5);
+        -- FireDeptLockers
+        table.insert(ProceduralDistributions.list["FireDeptLockers"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["FireDeptLockers"].items, 1);
 
-    -- PoliceLockers
-    table.insert(ProceduralDistributions.list["PoliceLockers"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["PoliceLockers"].items, 1);
+        -- LockerArmyBedroom
+        table.insert(ProceduralDistributions.list["LockerArmyBedroom"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["LockerArmyBedroom"].items, 1);
 
-    -- StoreShelfMedical
-    table.insert(ProceduralDistributions.list["StoreShelfMedical"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["StoreShelfMedical"].items, 5);
+        -- MedicalStorageTools
+        table.insert(ProceduralDistributions.list["MedicalStorageTools"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["MedicalStorageTools"].items, 1);
 
-    -- TestingLab
-    table.insert(ProceduralDistributions.list["TestingLab"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["TestingLab"].items, 1);
+        -- SecurityLockers
+        table.insert(ProceduralDistributions.list["SecurityLockers"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["SecurityLockers"].items, 0.5);
 
-    -- ToolStoreTools
-    table.insert(ProceduralDistributions.list["ToolStoreTools"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ToolStoreTools"].items, 0.25);
+        -- PoliceLockers
+        table.insert(ProceduralDistributions.list["PoliceLockers"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["PoliceLockers"].items, 1);
 
-    -- SurvivalGear
-    table.insert(ProceduralDistributions.list["SurvivalGear"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["SurvivalGear"].items, 0.25);
-    -- CrateCarpentry
-    table.insert(ProceduralDistributions.list["CrateCarpentry"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["CrateCarpentry"].items, 1);
+        -- StoreShelfMedical
+        table.insert(ProceduralDistributions.list["StoreShelfMedical"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["StoreShelfMedical"].items, 5);
 
-    -- CrateElectronics
-    table.insert(ProceduralDistributions.list["CrateCarpentry"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["CrateCarpentry"].items, 1);
+        -- TestingLab
+        table.insert(ProceduralDistributions.list["TestingLab"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["TestingLab"].items, 1);
 
-    -- GarageTools
-    table.insert(ProceduralDistributions.list["GarageTools"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["GarageTools"].items, 3);
+        -- ToolStoreTools
+        table.insert(ProceduralDistributions.list["ToolStoreTools"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ToolStoreTools"].items, 0.25);
 
-    -- MedicalClinicTools
-    table.insert(ProceduralDistributions.list["MedicalClinicTools"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["MedicalClinicTools"].items, 3);
+        -- SurvivalGear
+        table.insert(ProceduralDistributions.list["SurvivalGear"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["SurvivalGear"].items, 0.25);
+        -- CrateCarpentry
+        table.insert(ProceduralDistributions.list["CrateCarpentry"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["CrateCarpentry"].items, 1);
 
-    -- ToolStoreAccessories
-    table.insert(ProceduralDistributions.list["ToolStoreAccessories"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ToolStoreAccessories"].items, 3);
+        -- CrateElectronics
+        table.insert(ProceduralDistributions.list["CrateCarpentry"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["CrateCarpentry"].items, 1);
 
-    -- ArmyHangarTools
-    table.insert(ProceduralDistributions.list["ArmyHangarTools"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["ArmyHangarTools"].items, 3);
+        -- GarageTools
+        table.insert(ProceduralDistributions.list["GarageTools"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["GarageTools"].items, 3);
 
-    -- CrateTools
-    table.insert(ProceduralDistributions.list["CrateTools"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["CrateTools"].items, 3);
+        -- MedicalClinicTools
+        table.insert(ProceduralDistributions.list["MedicalClinicTools"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["MedicalClinicTools"].items, 3);
 
-    -- GymLockers
-    table.insert(ProceduralDistributions.list["GymLockers"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["GymLockers"].items, 1);
+        -- ToolStoreAccessories
+        table.insert(ProceduralDistributions.list["ToolStoreAccessories"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ToolStoreAccessories"].items, 3);
 
-    -- GunStoreShelf
-    table.insert(ProceduralDistributions.list["GunStoreShelf"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["GunStoreShelf"].items, 1);
+        -- ArmyHangarTools
+        table.insert(ProceduralDistributions.list["ArmyHangarTools"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["ArmyHangarTools"].items, 3);
 
-    -- BedroomSideTable
-    -- table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "Phun.WristWatch_Left_MilitaryX");
-    -- table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.25);
+        -- CrateTools
+        table.insert(ProceduralDistributions.list["CrateTools"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["CrateTools"].items, 3);
 
-    -- OfficeDrawers
-    table.insert(ProceduralDistributions.list["OfficeDrawers"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(ProceduralDistributions.list["OfficeDrawers"].items, 0.5);
-    -- glovebox
-    table.insert(VehicleDistributions["GloveBox"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(VehicleDistributions["GloveBox"].items, 0.1);
+        -- GymLockers
+        table.insert(ProceduralDistributions.list["GymLockers"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["GymLockers"].items, 1);
 
-    -- Suburbs
-    table.insert(SuburbsDistributions["all"]["inventorymale"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.05);
+        -- GunStoreShelf
+        table.insert(ProceduralDistributions.list["GunStoreShelf"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["GunStoreShelf"].items, 1);
 
-    table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "Phun.WristWatch_Left_MilitaryX");
-    table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.05);
+        -- BedroomSideTable
+        -- table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "Phun.WristWatch_Left_MilitaryX");
+        -- table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.25);
+
+        -- OfficeDrawers
+        table.insert(ProceduralDistributions.list["OfficeDrawers"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(ProceduralDistributions.list["OfficeDrawers"].items, 0.5);
+        -- glovebox
+        table.insert(VehicleDistributions["GloveBox"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(VehicleDistributions["GloveBox"].items, 0.1);
+
+        -- Suburbs
+        table.insert(SuburbsDistributions["all"]["inventorymale"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.05);
+
+        table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "Phun.WristWatch_Left_MilitaryX");
+        table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.05);
+    else
+        print(
+            "PhunSprintersUI items will NOT be added to loot spawns. Enable 'Add To Loot' in the mod options to add them.")
+    end
 end
+Events.OnPreDistributionMerge.Add(preDistributionMerge);
